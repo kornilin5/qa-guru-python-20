d2cd5b297b2e1dfb8bc6a8e9d672cbf03c699a6d
Kornilin5 <sasha_kornilin@mail.ru>
2024-03-29 20:02:00 +0400

    изменен conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,5 @@ import os
 from selene import browser
 
 DOMAIN_URL = 'https://demowebshop.tricentis.com/'
-USER_URL = DOMAIN_URL + '/users'
 LOGIN = os.getenv('LOGIN_USER')
 PASSWORD = os.getenv('PASSWORD_USER')
