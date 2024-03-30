43051bbc20adc7e9d908e7358e4aa361e87a600f
Kornilin5 <sasha_kornilin@mail.ru>
2024-03-30 19:02:37 +0400

    Изменена последовательность

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 import pytest
 from selene import browser
 
+load_dotenv()
 DOMAIN_URL = 'https://demowebshop.tricentis.com/'
 LOGIN = os.getenv('LOGIN_USER')
 PASSWORD = os.getenv('PASSWORD_USER')
-
-load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
